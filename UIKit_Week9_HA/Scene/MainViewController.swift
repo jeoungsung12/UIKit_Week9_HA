969c79fc0ca4484ba3957152c59d679ae7884743
jeoungsung12 <jeoungsung12@naver.com>
2025-02-20 16:38:49 +0900

    Initial Commit

---
+++ UIKit_Week9_HA/Scene/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  UIKit_Week9_HA
+//
+//  Created by 정성윤 on 2/20/25.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
